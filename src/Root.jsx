67eeed6e02085c5ba770257cb966418a436dc7d9
John Rey <johnreybaylen211995@gmail.com>
2022-11-29 17:19:50 +0800

    Remove semicolon

--- src/Root.jsx
+++ src/Root.jsx
@@ -1,17 +1,15 @@
-import React from 'react';
-import { Text, View } from 'react-native';
+import React from 'react'
+import { Text, View } from 'react-native'
 
-import HomeScreen from './screens/Home'
-import { SignLanguage } from './screens/SignLanguage'
+// import HomeScreen from './screens/Home'
+// import { SignLanguage } from './screens/SignLanguage'
 
 const Root = () => {
   return (
     <View>
-        <HomeScreen />
-        <SignLanguage />
-        <Text>Hello, I am John or</Text>
+
     </View>
-  );
+  )
 }
 
-export { Root };
+export { Root }
--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -1,10 +1,10 @@
-import React from 'react';
-import { Text } from 'react-native';
+import React from 'react'
+import { Text } from 'react-native'
 
 export default function Root () {
   return (
     <Text>Welcome Home</Text>
-  );
+  )
 }
 
 
